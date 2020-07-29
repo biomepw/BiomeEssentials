8e9d4d4de5934961b1fe0d3442f0ea81a33fbe59
Jordyn <jordyn@husk.pro>
2020-07-30 07:30:42 +0800

    add worldname to the coords command again

--- src/main/java/pw/biome/biomeessentials/commands/CoordsCommand.java
+++ src/main/java/pw/biome/biomeessentials/commands/CoordsCommand.java
@@ -14,8 +14,24 @@ public class CoordsCommand implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
+            String worldName = player.getWorld().getName();
+            String world = worldName;
+
+            switch (worldName) {
+                case "world_the_end":
+                    world = ChatColor.AQUA + "End";
+                    break;
+                case "world":
+                    world = ChatColor.YELLOW + "Overworld";
+                    break;
+                case "world_nether":
+                    world = ChatColor.RED + "Nether";
+                    break;
+            }
+
             Bukkit.broadcastMessage(ChatColor.YELLOW + player.getDisplayName() + "'s coords are "
-                    + player.getLocation().getBlockX() + ", " + player.getLocation().getBlockZ());
+                    + player.getLocation().getBlockX() + ", " + player.getLocation().getBlockY() + ", " +
+                    player.getLocation().getBlockZ() + ", World: " + world);
         }
         return true;
     }
