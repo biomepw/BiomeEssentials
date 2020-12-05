82b014b0ac86f1b8854b514646f37835fa6d1a46
Jordyn <me@huskehhh.com>
2020-12-06 09:47:59 +1100

    add debug and force

--- src/main/java/pw/biome/biomeessentials/commands/DisableSleepSkipCommand.java
+++ src/main/java/pw/biome/biomeessentials/commands/DisableSleepSkipCommand.java
@@ -1,13 +1,13 @@
 package pw.biome.biomeessentials.commands;
 
 import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.Default;
-import co.aikar.commands.annotation.Description;
+import co.aikar.commands.annotation.*;
 import lombok.Getter;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import pw.biome.biomeessentials.BiomeEssentials;
 
@@ -21,7 +21,6 @@ public class DisableSleepSkipCommand extends BaseCommand {
 
     @Getter
     private static final HashMap<UUID, Integer> preventListTaskMap = new HashMap<>();
-
     private static final HashSet<UUID> cooldownSet = new HashSet<>();
 
     public static boolean isSleepDisabled() {
@@ -60,12 +59,47 @@ public class DisableSleepSkipCommand extends BaseCommand {
 
                 taskId = Bukkit.getScheduler().runTaskLater(BiomeEssentials.getPlugin(), () ->
                         removeAndCheck(uuid), delay).getTaskId();
+
+                player.sendMessage(ChatColor.GOLD + "The night will be held for 3 minutes...");
             }
             preventListTaskMap.put(uuid, taskId);
 
             cooldownSet.add(uuid);
             Bukkit.getScheduler().runTaskLater(BiomeEssentials.getPlugin(), () -> cooldownSet.remove(uuid), 60 * 60 * 20); // 1 hr
         }
+    }
+
+    @Subcommand("force")
+    @CommandPermission("preventsleep.force")
+    public void forceSleep(Player player) {
+        Bukkit.getScheduler().runTaskLater(BiomeEssentials.getPlugin(), () -> {
+            long fullTime = player.getWorld().getFullTime();
+            long newTime = fullTime + (24000 - fullTime);
+
+            player.getWorld().setFullTime(newTime);
+
+            Bukkit.broadcastMessage(org.bukkit.ChatColor.YELLOW + player.getDisplayName() + org.bukkit.ChatColor.GOLD + " went to bed. Sweet Dreams");
+        }, 5 * 20);
+    }
+
+    @Subcommand("debug")
+    @CommandPermission("preventsleep.debug")
+    public void sleepDebug(CommandSender sender) {
+        if (preventListTaskMap.size() == 0) {
+            sender.sendMessage(ChatColor.GREEN + "No one is requesting night!");
+        } else {
+            sender.sendMessage(ChatColor.GOLD + "Players requesting sleep:");
+            preventListTaskMap.forEach((uuid, taskId) -> {
+                OfflinePlayer player = Bukkit.getOfflinePlayer(uuid);
+                sender.sendMessage(ChatColor.RED + player.getName());
+            });
+        }
+
+        sender.sendMessage(ChatColor.GOLD + "Players on cooldown for /preventsleep:");
+        cooldownSet.forEach(uuid -> {
+            OfflinePlayer player = Bukkit.getOfflinePlayer(uuid);
+            sender.sendMessage(ChatColor.RED + player.getName());
+        });
     }
 
     public static void removeAndCheck(UUID uuid) {
